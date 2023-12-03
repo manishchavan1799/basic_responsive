c59aa0456425060a88a41801ab5ad63df78b6390
Manish Chavan <manishchavan1799@gmail.com>
2023-12-03 14:14:07 +0530

    completed basic responsive website

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./styles/home.scss";
 import "./styles/footer.scss";
 import "./styles/contact.scss";
 import "./styles/mediaquery.scss"
-// import "./styles/mediaquery.scss";
+// import "./styles/mediaquery.scss";gi
 
 
 function App() {
